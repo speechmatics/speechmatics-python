2e311dee040b974da08c7959addb6312c236204e
pradeepk324 <123531125+pradeepk324@users.noreply.github.com>
2023-01-25 18:12:54 +0530

    Update constants.py
    Added Connection Close error types

--- speechmatics/constants.py
+++ speechmatics/constants.py
@@ -11,3 +11,12 @@ BATCH_SELF_SERVICE_URL = "https://asr.api.speechmatics.com/v2"
 #: The self-service realtime URL for non-enterprise customers.
 #: Note that it doesn't have the language added on the end.
 RT_SELF_SERVICE_URL = "wss://eu2.rt.speechmatics.com/v2"
+
+
+CONN_CLOSE_ERR_TYPES = ["protocol_error",
+                        "not_authorised",
+                        "invalid_model",
+                        "quota_exceeded",
+                        "timelimit_exceeded",
+                        "job_error",
+                        "internal_error"]
