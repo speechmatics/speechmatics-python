c8a863d3d578271ddda3ab082f91610ebed368e6
Dan Cochrane <danc@speechmatics.com>
2023-03-17 11:08:58 +0000

    Refactor class and format with black

--- speechmatics/metrics/normalizers/basic.py
+++ speechmatics/metrics/normalizers/basic.py
@@ -2,79 +2,90 @@ import re
 import unicodedata
 import regex
 
-# non-ASCII letters that are not separated by "NFKD" normalization
-ADDITIONAL_DIACRITICS = {
-    "œ": "oe",
-    "Œ": "OE",
-    "ø": "o",
-    "Ø": "O",
-    "æ": "ae",
-    "Æ": "AE",
-    "ß": "ss",
-    "ẞ": "SS",
-    "đ": "d",
-    "Đ": "D",
-    "ð": "d",
-    "Ð": "D",
-    "þ": "th",
-    "Þ": "th",
-    "ł": "l",
-    "Ł": "L",
-}
-
-
-def remove_symbols_and_diacritics(s: str, keep=""):
-    """
-    Replace any other markers, symbols, and punctuations with a space,
-    and drop any diacritics (category 'Mn' and some manual mappings)
-    """
-    return "".join(
-        c
-        if c in keep
-        else ADDITIONAL_DIACRITICS[c]
-        if c in ADDITIONAL_DIACRITICS
-        else ""
-        if unicodedata.category(c) == "Mn"
-        else " "
-        if unicodedata.category(c)[0] in "MSP"
-        else c
-        for c in unicodedata.normalize("NFKD", s)
-    )
-
-
-def remove_symbols(s: str):
-    """
-    Replace any other markers, symbols, punctuations with a space, keeping diacritics
-    """
-    return "".join(
-        " " if unicodedata.category(c)[0] in "MSP" else c for c in unicodedata.normalize("NFKC", s)
-    )
-
 
 class BasicTextNormalizer:
-
     def __init__(self, remove_diacritics: bool = False, split_letters: bool = False):
-        self.clean = remove_symbols_and_diacritics if remove_diacritics else remove_symbols
+        self.clean = (
+            self.remove_symbols_and_diacritics
+            if remove_diacritics
+            else self.remove_symbols
+        )
         self.split_letters = split_letters
+
+        # non-ASCII letters that are not separated by "NFKD" normalization
+        self.additional_diacritics = {
+            "œ": "oe",
+            "Œ": "OE",
+            "ø": "o",
+            "Ø": "O",
+            "æ": "ae",
+            "Æ": "AE",
+            "ß": "ss",
+            "ẞ": "SS",
+            "đ": "d",
+            "Đ": "D",
+            "ð": "d",
+            "Ð": "D",
+            "þ": "th",
+            "Þ": "th",
+            "ł": "l",
+            "Ł": "L",
+        }
+
+    def remove_symbols_and_diacritics(self, s: str, keep=""):
+        """
+        Replace any other markers, symbols, and punctuations with a space,
+        and drop any diacritics (category 'Mn' and some manual mappings)
+        """
+        return "".join(
+            c
+            if c in keep
+            else self.additional_diacritics[c]
+            if c in self.additional_diacritics
+            else ""
+            if unicodedata.category(c) == "Mn"
+            else " "
+            if unicodedata.category(c)[0] in "MSP"
+            else c
+            for c in unicodedata.normalize("NFKD", s)
+        )
+
+    def remove_symbols(self, s: str):
+        """
+        Replace any other markers, symbols, punctuations with a space, keeping diacritics
+
+        Args:
+            s (str): raw input transcript
+
+        Returns:
+            s (str): same string which has been modified inplace
+        """
+        return "".join(
+            " " if unicodedata.category(c)[0] in "MSP" else c
+            for c in unicodedata.normalize("NFKC", s)
+        )
 
     def __call__(self, s: str) -> str:
         """
-        Take in a string.
-        Return a normalised string.
+        Return a normalised string, given an input string, using the following modifications:
 
-        Make everything lowercase.
-        Replace diacritics with the ASCII eqivalent.
-        Remove tokens between brackets.
-        Replace whitespace and any remaining punctuation with single space.
+            Makes everything lowercase.
+            Remove tokens between brackets.
+            Replace diacritics with the ASCII eqivalent.
+            Replace whitespace and any remaining punctuation with single space.
         """
+
         s = s.lower()
-        s = re.sub(r"[<\[][^>\]]*[>\]]", "", s)  # remove words between brackets
-        s = re.sub(r"\(([^)]+?)\)", "", s)  # remove words between parenthesis
+
+        s = re.sub(r"[<\[][^>\]]*[>\]]", "", s)
+        s = re.sub(r"\(([^)]+?)\)", "", s)
+
         s = self.clean(s).lower()
 
+        # insert a single space between characters in a string
         if self.split_letters:
             s = " ".join(regex.findall(r"\X", s, regex.U))
 
-        s = re.sub(r"\s+", " ", s)  # replace any successive whitespace characters with a space
+        s = re.sub(r"\s+", " ", s)
 
         return s
