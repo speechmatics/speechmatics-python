978ce77d6cffb99dcf195ca128a3181b9ff9ffc1
Dan Cochrane <danc@speechmatics.com>
2023-11-09 12:26:45 +0000

    Init Commit

---
+++ metrics/cli.py
@@ -0,0 +1,34 @@
+"""Entrypoint for SM metrics"""
+import argparse
+
+import metrics.diarization.sm_diarization_metrics.cookbook as diarization_metrics
+import metrics.wer as wer_metrics
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Your CLI description")
+
+    # Create subparsers
+    subparsers = parser.add_subparsers(dest="mode")
+    subparsers.required = True  # Make sure a subparser id always provided
+
+    wer_parser = subparsers.add_parser("wer", help="Entrypoint for WER metrics")
+    wer_metrics.get_wer_args(wer_parser)
+
+    diarization_parser = subparsers.add_parser(
+        "diarization", help="Entrypoint for diarization metrics"
+    )
+    diarization_metrics.get_diarization_args(diarization_parser)
+
+    args = parser.parse_args()
+
+    if args.mode == "wer":
+        wer_metrics.main(args)
+    elif args.mode == "diarization":
+        diarization_metrics.main(args)
+    else:
+        print("Invalid script specified")
+
+
+if __name__ == "__main__":
+    main()
